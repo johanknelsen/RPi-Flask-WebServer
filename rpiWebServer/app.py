1c62ecd0e51e7f8b8ca384c4af8e749af4188d7b
johanknelsen@gmail.com <johanknelsen@gmail.com>
2020-03-30 21:03:56 +0100

    switched the droplet delays to a scheduled

--- rpiWebServer/app.py
+++ rpiWebServer/app.py
@@ -5,6 +5,10 @@ import RPi.GPIO as GPIO
 from flask import Flask, render_template, flash, request
 from flask_bootstrap import Bootstrap
 from time import sleep
+import sched, time
+
+
+s = sched.scheduler(time.time, time.sleep)
 
 app = Flask(__name__, instance_relative_config=False)
 Bootstrap(app)
@@ -24,7 +28,7 @@ ledYlw = 19
 ledGrn = 26
 
 shutter = 17
-solonoid = 27
+solenoid = 27
 
 #initialize GPIO status variables
 buttonSts = 0
@@ -46,7 +50,7 @@ GPIO.setup(ledYlw, GPIO.OUT)
 GPIO.setup(ledGrn, GPIO.OUT)
 
 GPIO.setup(shutter, GPIO.OUT)
-GPIO.setup(solonoid, GPIO.OUT)
+GPIO.setup(solenoid, GPIO.OUT)
 
 # turn leds OFF 
 GPIO.output(ledRed, GPIO.LOW)
@@ -54,7 +58,27 @@ GPIO.output(ledYlw, GPIO.LOW)
 GPIO.output(ledGrn, GPIO.LOW)
 
 GPIO.output(shutter, GPIO.HIGH)
-GPIO.output(solonoid, GPIO.HIGH)
+GPIO.output(solenoid, GPIO.HIGH)
+
+#Defining solenoid and shutter states
+
+def solenoid_open(a='default'):
+    GPIO.output(solenoid, GPIO.LOW)
+#    print('Solenoid open', time.time(), a)
+
+def solenoid_closed(a='default'):
+    GPIO.output(solenoid, GPIO.HIGH)
+#    print('Solenoid closed', time.time(), a)
+
+def shutter_triggered(a='default'):
+	GPIO.output(shutter, GPIO.LOW)
+#    print('Shutter triggered', time.time(), a)
+
+def shutter_reset(a='default'):
+	GPIO.output(shutter, GPIO.HIGH)
+#	print('Shutter triggered', time.time(), a)
+
+#end of solenoid and shutter states
 
 @app.route("/")
 def index():
@@ -92,7 +116,7 @@ def action(deviceName, action):
 		actuator = shutter
 
 	if deviceName == 'solonoid':
-		actuator = solonoid
+		actuator = solenoid
    
 	if action == "on":
 		GPIO.output(actuator, GPIO.HIGH)
@@ -120,27 +144,40 @@ def action(deviceName, action):
 def droplet(droplet):
 	shutter_delay = list(range(5, 40))
 
-	dropdelay =  round(float(request.form['btwndrops'])/100,3)
+	dropdelay =  round(float(request.form['btwndrops'])/100,4)
+	dropclosed = dropdelay + .01
 
-	delay_amnt =  round(float(request.form['shutterdelay'])/100,3)
+	delay_amnt =  round(float(request.form['shutterdelay'])/100,4)
+	shutterclosed = delay_amnt + 0.3
+	
+	s.enter(0.01,.05 , solenoid_open, kwargs={'a': 'drop 1 open'})
+	s.enter(.05,0.01, solenoid_closed, kwargs={'a': 'drop 1 closed'})
+	
+	s.enter(dropdelay,0.05, solenoid_open, kwargs={'a': 'drop 2 open'})
+	s.enter(dropclosed,.01, solenoid_closed, kwargs={'a': 'drop 2 closed'})
+	
+	s.enter(delay_amnt,0.3, shutter_triggered, kwargs={'a': 'shutter triggered'})
+	s.enter(shutterclosed,0.01, shutter_reset, kwargs={'a': 'Shutter reset'})
+	
+	s.run()
 
-	#first drop
-	GPIO.output(solonoid, GPIO.LOW)
-	sleep(0.06)
-	GPIO.output(solonoid, GPIO.HIGH)
-	#delay for second drop
-	sleep(dropdelay)
-	#second drop
-	GPIO.output(solonoid, GPIO.LOW)
-	sleep(0.06)
-	GPIO.output(solonoid, GPIO.HIGH)
-	#Shutter triggered
-	sleep(delay_amnt)
-	GPIO.output(shutter, GPIO.LOW)
-	#GPIO pins reset
-	sleep(0.30)
-	GPIO.output(shutter, GPIO.HIGH)
-	GPIO.output(solonoid, GPIO.HIGH)
+	# #first drop
+	# GPIO.output(solonoid, GPIO.LOW)
+	# sleep(0.06)
+	# GPIO.output(solonoid, GPIO.HIGH)
+	# #delay for second drop
+	# sleep(dropdelay)
+	# #second drop
+	# GPIO.output(solonoid, GPIO.LOW)
+	# sleep(0.06)
+	# GPIO.output(solonoid, GPIO.HIGH)
+	# #Shutter triggered
+	# sleep(delay_amnt)
+	# GPIO.output(shutter, GPIO.LOW)
+	# #GPIO pins reset
+	# sleep(0.30)
+	# GPIO.output(shutter, GPIO.HIGH)
+	# GPIO.output(solonoid, GPIO.HIGH)
 
 
 
@@ -149,6 +186,9 @@ def droplet(droplet):
 	dropdown_list=shutter_delay, 
 	shutterdelay=delay_amnt, 
 	btwndrops=dropdelay)
+
+
+
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
